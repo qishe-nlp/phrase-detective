f6bafe1cdcb8a8df97210c75a81115a83a9d3e8f
JingWeiLyu <murka@JingWeiLyus-MBP.powter>
2021-05-23 13:57:24 +0800

    update version of apidocs

--- apidocs/source/conf.py
+++ apidocs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Phoenix Grey'
 author = 'Phoenix Grey'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.11'
+release = '0.1.12'
 
 
 # -- General configuration ---------------------------------------------------
