a41a3f2e46a5c3489f0a6ccb78653f4ca3eeef82
JingWeiLyu <murka@JingWeiLyus-MBP.powter>
2024-12-25 19:08:10 +0800

    fix version

--- phrase_detective/__init__.py
+++ phrase_detective/__init__.py
@@ -52,7 +52,7 @@ Components for ``spacy`` pipeline, to detect noun phrase, prep phrase, verb rela
 
 """
 
-__version__ = '0.1.33'
+__version__ = '0.1.35'
 
 from .noun_phrases import NounPhraseRecognizer
 from .verb_knowledge import VerbKnowledgeRecognizer
